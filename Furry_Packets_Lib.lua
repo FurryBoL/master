931fd8709c0a4d3ed283b513ffa91a99ea08dd67
FurryBoL <treeman8612@gmail.com>
2015-12-10 12:04:25 +1300

    Update Furry_Packets_Lib.lua

--- Furry_Packets_Lib.lua
+++ Furry_Packets_Lib.lua
@@ -1,6 +1,6 @@
 -- Furry Packets Lib
 -- by Furry
--- Version 5.24
+-- Version 6.0
 
 _AUTO_UPDATE_FURRY = true -- Set this to false to prevent automatic updates
 
@@ -8,12 +8,12 @@ _AUTO_UPDATE_FURRY = true -- Set this to false to prevent automatic updates
 --  There is no ChangeLog, this is just a script to keep AutoLeveler and SkinChanger Packets all in one place, so when I need to update them, all I need to do is update this script.
 --			[ ChangeLog ]
 
-_GAME_VERSION = string.find(GetGameVersion(), 'Releases/5.23') -- Change this after a patch if you want errors and bugsplats :)
-_GAME_VERSION_LEVELER = string.find(GetGameVersion(), 'Releases/5.23') -- Change this after a patch if you want errors and bugsplats :)
+_GAME_VERSION = string.find(GetGameVersion(), 'Releases/5.24') -- Change this after a patch if you want errors and bugsplats :)
+_GAME_VERSION_LEVELER = string.find(GetGameVersion(), 'Releases/5.24') -- Change this after a patch if you want errors and bugsplats :)
 
 local serveradress = "raw.githubusercontent.com"
 local scriptadress = "/FurryBoL/master/master"
-local LocalVersion = "5.24"
+local LocalVersion = "6.0"
  
  
 if _AUTO_UPDATE_FURRY or true then
@@ -44,19 +44,19 @@ print("<font color='#9900FF'>[Furry Packets Lib] </font><font color='#FF0000'>-<
 if (_GAME_VERSION_LEVELER ~= nil) then
 	_G.LevelSpell = function(id)
 		local offsets = { 
-			[_Q] = 0x61,
-			[_W] = 0x81,
-			[_E] = 0xA1,
-			[_R] = 0xC1,
+			[_Q] = 0x1E,
+			[_W] = 0xD3,
+			[_E] = 0x3A,
+			[_R] = 0xA8,
 		}
-		local p = CLoLPacket(0x0033)
-		p.vTable = 0xEECE14
+		local p = CLoLPacket(0x00B6)
+		p.vTable = 0xFE3124
 		p:EncodeF(myHero.networkID)
-		p:Encode1(0x73)
-		for i = 1, 4 do p:Encode1(0xF9) end
-		for i = 1, 4 do p:Encode1(0x1E) end
+		p:Encode1(0xC1)
 		p:Encode1(offsets[id])
-		for i = 1, 4 do p:Encode1(0x99) end
+		for i = 1, 4 do p:Encode1(0x63) end
+		for i = 1, 4 do p:Encode1(0xC5) end
+		for i = 1, 4 do p:Encode1(0x6A) end
 		for i = 1, 4 do p:Encode1(0x00) end
 		SendPacket(p)
 	end
@@ -74,50 +74,49 @@ end
 if (_GAME_VERSION ~= nil) then
 	skinsPB = {
 		[1] = nil,
-		[2] = 0x74,
-		[3] = 0xF4,
-		[4] = 0xB4,
-		[5] = 0x34,
-		[6] = 0x54,
-		[7] = 0xD4,
-		[8] = 0x94,
-		[9] = 0x14,
-		[10] = 0x84,
-		[11] = 0x44,
-		[12] = 0xC4,
-		[13] = 0x04,
+		[2] = 0xCA,
+		[3] = 0x0C,
+		[4] = 0x4C,
+		[5] = 0xF8,
+		[6] = 0xB8,
+		[7] = 0x78,
+		[8] = 0x38,
+		[9] = 0xE8,
+		[10] = 0xA8,
+		[11] = 0x68,
+		[12] = 0x28,
+		[13] = 0xD8,
 	}
-	skinObjectPos = 11
-	skinHeader = 0x13
-	dispellHeader = 0x13B
-	skinH = 0x74
-	skinHPos = 11
+	skinObjectPos = 6
+	skinHeader = 0x3A
+	dispellHeader = 0xB7
+	skinH = 0x8C
+	skinHPos = 32
 end
 
 function SendSkinPacket(mObject, skinPB, networkID)
 	if (_GAME_VERSION ~= nil) then
-		local mP = CLoLPacket(0x13)
-		mP.vTable = 0xF4FDE0
+		local mP = CLoLPacket(0x3A)
+		mP.vTable = 0xF351B0
 		mP:EncodeF(myHero.networkID)
-		mP:Encode4(0x00000000)
-		mP:Encode1(0x00)
-		if (skinPB == nil) then
-			mP:Encode4(0x2F2F2F2F)
-		else
-			mP:Encode1(skinPB)
-			for I = 1, 3 do
-				mP:Encode1(0x74)
-			end
-			end
-		mP:Encode1(0x75)
 		for I = 1, string.len(mObject) do
-			mP:Encode1(string.byte(string.sub(mObject, I, I)))
+			mP:Encode1(string.byte(string.sub(mObject, I, I)));
 		end
 		for I = 1, (16 - string.len(mObject)) do
 			mP:Encode1(0x00)
 		end
-		mP:Encode4(0x00000000)
+		mP:Encode4(0x0000000E)
 		mP:Encode4(0x0000000F)
+		mP:Encode2(0x0000)
+		if (skinPB == nil) then
+			mP:Encode4(0x82828282)
+		else
+			mP:Encode1(skinPB)
+			for I = 1, 3 do
+				mP:Encode1(skinH)
+			end
+		end
+		mP:Encode4(0x00000000)
 		mP:Encode4(0x00000000)
 		mP:Encode1(0x00)
 		mP:Hide()
