32e8d3a77a7c22c0af3b622f6bf304a535655d68
FurryBoL <treeman8612@gmail.com>
2016-01-25 12:15:38 +1300

    Update Furry_Packets_Lib.lua

--- Furry_Packets_Lib.lua
+++ Furry_Packets_Lib.lua
@@ -1,6 +1,6 @@
 -- Furry Packets Lib
 -- by Furry
--- Version 6.0
+-- Version 6.1
 
 _AUTO_UPDATE_FURRY = true -- Set this to false to prevent automatic updates
 
@@ -8,14 +8,13 @@ _AUTO_UPDATE_FURRY = true -- Set this to false to prevent automatic updates
 --  There is no ChangeLog, this is just a script to keep AutoLeveler and SkinChanger Packets all in one place, so when I need to update them, all I need to do is update this script.
 --			[ ChangeLog ]
 
-_GAME_VERSION = string.find(GetGameVersion(), 'Releases/5.24') -- Change this after a patch if you want errors and bugsplats :)
-_GAME_VERSION_LEVELER = string.find(GetGameVersion(), 'Releases/5.24') -- Change this after a patch if you want errors and bugsplats :)
+_GAME_VERSION = string.find(GetGameVersion(), 'Releases/6.1') -- Change this after a patch if you want errors and bugsplats :)
+_GAME_VERSION_LEVELER = string.find(GetGameVersion(), 'Releases/6.1') -- Change this after a patch if you want errors and bugsplats :)
 
 local serveradress = "raw.githubusercontent.com"
 local scriptadress = "/FurryBoL/master/master"
-local LocalVersion = "6.0"
- 
- 
+local LocalVersion = "6.1"
+
 if _AUTO_UPDATE_FURRY or true then
 	local ServerVersionDATA = GetWebResult(serveradress , scriptadress.."/Furry_Packets_Lib.version")
 	if ServerVersionDATA then
@@ -43,22 +42,30 @@ print("<font color='#9900FF'>[Furry Packets Lib] </font><font color='#FF0000'>-<
 
 if (_GAME_VERSION_LEVELER ~= nil) then
 	_G.LevelSpell = function(id)
-		local offsets = { 
-			[_Q] = 0x1E,
-			[_W] = 0xD3,
-			[_E] = 0x3A,
-			[_R] = 0xA8,
-		}
-		local p = CLoLPacket(0x00B6)
-		p.vTable = 0xFE3124
-		p:EncodeF(myHero.networkID)
-		p:Encode1(0xC1)
-		p:Encode1(offsets[id])
-		for i = 1, 4 do p:Encode1(0x63) end
-		for i = 1, 4 do p:Encode1(0xC5) end
-		for i = 1, 4 do p:Encode1(0x6A) end
-		for i = 1, 4 do p:Encode1(0x00) end
-		SendPacket(p)
+	local offsets = { 
+		[_Q] = 0x71,
+		[_W] = 0xF1,
+		[_E] = 0x31,
+		[_R] = 0xB1,
+	}
+	local p = CLoLPacket(0x00DB)
+	p.vTable = 0xF6D830
+	p:EncodeF(myHero.networkID)
+	for i = 1, 4 do
+		p:Encode1(0x30)
+	end
+	p:Encode1(0x17)
+	for i = 1, 4 do
+		p:Encode1(0x81)
+	end
+	for i = 1, 4 do
+		p:Encode1(0x6A)
+	end
+	p:Encode1(offsets[id])
+	for i = 1, 4 do
+		p:Encode1(0x00)
+	end
+	SendPacket(p)
 	end
 end
 
@@ -74,51 +81,53 @@ end
 if (_GAME_VERSION ~= nil) then
 	skinsPB = {
 		[1] = nil,
-		[2] = 0xCA,
-		[3] = 0x0C,
-		[4] = 0x4C,
-		[5] = 0xF8,
-		[6] = 0xB8,
-		[7] = 0x78,
-		[8] = 0x38,
-		[9] = 0xE8,
-		[10] = 0xA8,
-		[11] = 0x68,
-		[12] = 0x28,
-		[13] = 0xD8,
+		[2] = 0x74,
+		[3] = 0xBC,
+		[4] = 0xA2,
+		[5] = 0xB2,
+		[6] = 0x2B,
+		[7] = 0x3B,
+		[8] = 0x27,
+		[9] = 0x37,
+		[10] = 0x2F,
+		[11] = 0x3F,
+		[12] = 0xA0,
+		[13] = 0xB0,
 	}
-	skinObjectPos = 6
-	skinHeader = 0x3A
-	dispellHeader = 0xB7
-	skinH = 0x8C
-	skinHPos = 32
+	skinObjectPos = 16
+	skinHeader = 0x9C
+	dispellHeader = 0x39
+	skinH = 0xAC
+	skinHPos = 6
 end
 
 function SendSkinPacket(mObject, skinPB, networkID)
 	if (_GAME_VERSION ~= nil) then
-		local mP = CLoLPacket(0x3A)
-		mP.vTable = 0xF351B0
+		local mP = CLoLPacket(skinHeader)
+		mP.vTable = 0xEECD38
 		mP:EncodeF(myHero.networkID)
-		for I = 1, string.len(mObject) do
-			mP:Encode1(string.byte(string.sub(mObject, I, I)));
+		if (skinPB == nil) then
+			mP:Encode4(0xA4A4A4A4)
+		else
+		mP:Encode1(skinPB)
+		for I = 1, 3 do
+			mP:Encode1(skinH)
 		end
-		for I = 1, (16 - string.len(mObject)) do
+		end
+		mP:Encode4(0x00000000)
+		mP:Encode2(0x0000)
+		mP:Encode1(0x00)
+		for I = 1, string.len(mObject) do
+			mP:Encode1(string.byte(string.sub(mObject, I, I)))
+		end
+		for I = 1, (14 - string.len(mObject)) do
 			mP:Encode1(0x00)
 		end
-		mP:Encode4(0x0000000E)
-		mP:Encode4(0x0000000F)
-		mP:Encode2(0x0000)
-		if (skinPB == nil) then
-			mP:Encode4(0x82828282)
-		else
-			mP:Encode1(skinPB)
-			for I = 1, 3 do
-				mP:Encode1(skinH)
-			end
-		end
-		mP:Encode4(0x00000000)
-		mP:Encode4(0x00000000)
 		mP:Encode1(0x00)
+		mP:Encode1(0x00)
+		mP:Encode4(0x0000000D)
+		mP:Encode4(0x0000000F)
+		mP:Encode4(0x00000000)
 		mP:Hide()
 		RecvPacket(mP)
 	end
